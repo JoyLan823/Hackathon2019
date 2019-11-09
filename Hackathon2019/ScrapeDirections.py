63f03f9a68a6c7987ad75e58e22086a0ad7636d0
Joy.Lan <joylan@brandeis.edu>
2019-11-09 16:41:28 -0500

    took out api key

--- Hackathon2019/ScrapeDirections.py
+++ Hackathon2019/ScrapeDirections.py
@@ -2,10 +2,10 @@ import os
 import json
 import urllib
 
-url = 'https://maps.googleapis.com/maps/api/directions/json?origin=Disneyland&destination=Universal%20Studios%20Hollywood&mode=walking&key=AIzaSyBfFAGyGioO5PDmoOmF-G_7RT6pV8AtR8o'
+url = 'https://maps.googleapis.com/maps/api/directions/json?origin=Disneyland&destination=Universal%20Studios%20Hollywood&mode=walking&key=YOURKEY'
 
 jsonURL = urllib.urlopen(url)
 json_data = json.loads(jsonURL.read())       #Stores entire JSON object fetched from the REST endpoint
-print (json_data)
+json_data = json_data.get('routes').get('legs')
 with open('directions_json.json', 'w') as write_file:
     json.dump(json_data, write_file, indent=2)
